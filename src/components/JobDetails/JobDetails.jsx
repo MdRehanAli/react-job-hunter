29fc2a7c1b2ec39660bf052632a65e94f6b6c059
MdRehanAli <rehanali21d@gmail.com>
2024-03-30 20:43:01 +0600

    Added Job details in details

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -20,16 +20,24 @@ const JobDetails = () => {
 
     return (
         <div>
-            <h2>Job Details: {id}</h2>
-            <div className="grid gap-4 md:grid-cols-4 ">
-                <div className="border md:col-span-3">
+            <div className="grid gap-4 md:grid-cols-3 ">
+                <div className="border md:col-span-2">
                     <p><b>Job Description: </b>{job.job_description}</p>
 
                     <p><b>Job Responsibility: </b>{job.job_responsibility}</p>
+                    <p><b>Educational Requirements: </b>{job.educational_requirements}</p>
+                    <p><b>Experiences: </b>{job.experiences}</p>
                 </div>
                 <div className="border ">
                     <h2 className="text-2xl">Job Details</h2>
-                    <button onClick={()=> handleApplyJob()} className="btn btn-primary">Apply</button>
+                    <p><b>Salary: </b>{job.salary}</p>
+                    <p><b>Job title: </b>{job.job_title}</p>
+                    <p>Contact Information</p>
+                    <p><b>Phone: </b>{job.phone}</p>
+                    <p><b>Email: </b>{job.email}</p>
+                    <p><b>Address: </b>{job.address}</p>
+
+                    <button onClick={()=> handleApplyJob()} className="btn btn-primary w-full">Apply</button>
                 </div>
             </div>
             <ToastContainer />
